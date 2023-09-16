ce5ae838be7de2694e77e67b43d7b505e663b4c2
Davc1910 <davc1910@hotmail.com>
2023-09-16 08:45:46 -0400

    Se agregó un darboard semi-completo en la vista Inicio.

--- resources/views/home/inicio.blade.php
+++ resources/views/home/inicio.blade.php
@@ -9,15 +9,163 @@
                     <!-- ? Tabla o formulario con sus respectivos campos -->
 
           <!-- Recent Sales Start -->
-          @include('partials.messages')
-            <div class="container-fluid pt-4 px-4">
+
+           @include('partials.messages')
+                <div class="container-fluid pt-4 px-4">
+                    <div class="row">
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="rounded d-flex align-items-center justify-content-between p-4" style="background:  rgb(255, 253, 253);">
+                                <i class="fa fa-university fa-3x text-dark"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">SEDE</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="rounded d-flex align-items-center justify-content-between p-4" style="background: rgb(255, 253, 253);">
+                                <i class="fa fa-chart-bar fa-3x text-primary"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">División</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-xl-3">
+                            <div class=" rounded d-flex align-items-center justify-content-between p-4" style="background: rgb(255, 253, 253);">
+                                <i class="fa fa-chart-area fa-3x text-primary"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">Cargo</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="rounded d-flex align-items-center justify-content-between p-4" style="background: rgb(255, 253, 253);">
+                                <i class="fa fa-chart-pie fa-3x text-primary"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">Persona</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container-fluid pt-4 px-4">
+                    <div class="row">
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="rounded d-flex align-items-center justify-content-between p-4" style="background:  rgb(255, 253, 253);">
+                                <i class="fa fa-university fa-3x text-dark"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">Marca</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="rounded d-flex align-items-center justify-content-between p-4" style="background: rgb(255, 253, 253);">
+                                <i class="fa fa-chart-bar fa-3x text-primary"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">Modelo</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-xl-3">
+                            <div class=" rounded d-flex align-items-center justify-content-between p-4" style="background: rgb(255, 253, 253);">
+                                <i class="fa fa-chart-area fa-3x text-primary"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">Tipos de Periférico</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="rounded d-flex align-items-center justify-content-between p-4" style="background: rgb(255, 253, 253);">
+                                <i class="fa fa-chart-pie fa-3x text-primary"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">Periférico</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container-fluid pt-4 px-4">
+                    <div class="row">
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="rounded d-flex align-items-center justify-content-between p-4" style="background:  rgb(255, 253, 253);">
+                                <i class="fa fa-university fa-3x text-dark"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">Sistemas Operativos</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="rounded d-flex align-items-center justify-content-between p-4" style="background: rgb(255, 253, 253);">
+                                <i class="fa fa-chart-bar fa-3x text-primary"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">Equipos Informáticos</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-xl-3">
+                            <div class=" rounded d-flex align-items-center justify-content-between p-4" style="background: rgb(255, 253, 253);">
+                                <i class="fa fa-chart-area fa-3x text-primary"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">Equpios Asignados</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="rounded d-flex align-items-center justify-content-between p-4" style="background: rgb(255, 253, 253);">
+                                <i class="fa fa-chart-pie fa-3x text-primary"></i>
+                                <div class="ms-3">
+                                    <p class="mb-2">Equipos DesicorporadosÑ</p>
+                                    <h6 class="mb-0">$1234</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            {{-- Catalogo para la Estadistíca  --}}
+                {{-- <div class="container-fluid pt-4 px-4">
+                    <div class="row g-4">
+                        <div class="col-sm-12 col-xl-6">
+                            <div class="bg-secondary text-center rounded p-4">
+                                <div class="d-flex align-items-center justify-content-between mb-4">
+                                    <h6 class="mb-0">Worldwide Sales</h6>
+                                    <a href="">Show All</a>
+                                </div>
+                                <canvas id="worldwide-sales"></canvas>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-xl-6">
+                            <div class="bg-secondary text-center rounded p-4">
+                                <div class="d-flex align-items-center justify-content-between mb-4">
+                                    <h6 class="mb-0">Salse & Revenue</h6>
+                                    <a href="">Show All</a>
+                                </div>
+                                <canvas id="salse-revenue"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div> --}}
+
+            {{-- <div class="container-fluid pt-4 px-4">
                 <div class="row g-4">
                     <div class="col-sm-12 col-xl-13">
                         <div class="">
-                            <div class="row">
+                            <div class="row"> --}}
 
                                 {{-- Primera Carta --}}
-                                <div class="card mb-3" style="width: 20% ; left: 5%; margin-top: 1% ;">
+                                {{-- <div class="card mb-3" style="width: 20% ; left: 5%; margin-top: 1% ;">
                                     <div class="card-body">
                                         <img src="img/descarga.jpg" class="card-img-top" alt="" height=" 80%; ">
                                         <center>
@@ -28,10 +176,10 @@
                                         colectiva, integral, continua y permanente de los trabajadores y las trabajadoras, orientada al desarrollo de sus capacidades para la producción de bienes
                                         y prestación de servicios que satisfagan las necesidades del Poder Popular".
                                     </p>
-                                </div>
+                                </div> --}}
 
                                 {{-- Segunda Carta --}}
-                                <div class="card mb-3" style="width: 20% ; left: 8%; margin-top: 1% ;">
+                                {{-- <div class="card mb-3" style="width: 20% ; left: 8%; margin-top: 1% ;">
                                     <div class="card-body">
                                         <center>
                                             <h3 class="card-title" style="color: black;">Visión</h3>
@@ -42,10 +190,10 @@
                                         </p>
                                         <img src="img/banner_comunidad_inces.png" class="card-img-top" alt="" style="height: 37% ;">
                                     </div>
-                                </div>
+                                </div> --}}
 
                                 {{-- Tercera Carta --}}
-                                <div class="card mb-3" style="width: 20% ; left: 11%; margin-top: 1% ;">
+                                {{-- <div class="card mb-3" style="width: 20% ; left: 11%; margin-top: 1% ;">
                                     <div class="card-body">
                                         <img src="img/historia.jpg" class="card-img-top" alt="" height=" 60%; ">
                                         <center>
@@ -56,10 +204,10 @@
                                         cuando se creó el Instituto Nacional de Cooperación Educativa (Ince), 
                                         el proyecto de Ley de Creación del instituto estuvo a cargo del maestro Luis Beltrán Prieto Figueroa".
                                     </p>
-                                </div>
+                                </div> --}}
                                 
                                 {{-- Cuarta Carta --}}
-                                <div class="card mb-3" style="width: 20% ; left: 14%; margin-top: 1% ;">
+                                {{-- <div class="card mb-3" style="width: 20% ; left: 14%; margin-top: 1% ;">
                                     <div class="card-body">
                                         <center>
                                             <h3 class="card-title" style="color: black;">Valores</h3>
@@ -75,7 +223,7 @@
                         </div>
                     </div> 
                 </div>
-            </div>
+            </div> --}} 
           <!-- Recent Sales End -->
                                                 
 <!-- Form End -->
